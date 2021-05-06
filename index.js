5dac0108af73cf32ed0ff194eaa2e2207e859e05
jessica you <jessicaxytan@gmail.com>
2021-05-06 12:28:05 -0400

    commit tree 26dc984db7ed79feb7582f2bf3c3e3d52ac028ea

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// "before each" hook for "returns an employee with the original key value pairs and the new key value pair":
+const employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway'
+}
+
+function  updateEmployeeWithKeyAndValue(employee, key, value) {
+    return Object.assign({}, employee, {[key]:value})
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
